1eddbc094c9e6376bfb3e126de5c8dcef0af48b0
CW01\uig02071 <aldo.leon.h@continental-corporation.com>
2024-02-13 19:40:44 -0600

    adding some extra files done in the course

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# driver.close()
+driver.quit()
---
+++ newTurtle.py
@@ -0,0 +1,8 @@
+# SNAKE GAME
+from turtle import Screen, Turtle
+s=Screen()
+s.setup(600,600)
+s.bgcolor("black")
+s.title("Snake")
+
+s.exitonclick()
